49e0c867e1089eb53bc52460715aa34db8b263ac
Christopher White <cwhite01+github@gmail.com>
2018-01-12 15:05:39 +0100

    Applicatielagen

--- groenetenen/src/main/java/be/vdab/web/FiliaalController.java
+++ groenetenen/src/main/java/be/vdab/web/FiliaalController.java
@@ -1,14 +1,29 @@
 package be.vdab.web;
 
+import java.util.logging.Logger;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import be.vdab.services.FiliaalService;
 
 @Controller
 @RequestMapping("/filialen")
 class FiliaalController {
+	private static final String REDIRECT_URL_NA_TOEVOEGEN = "redirect:/filialen";
+	private static final Logger LOGGER =
+	Logger.getLogger(FiliaalController.class.getName());
+	// importeer Logger uit de package java.util.logging
 private static final String FILIALEN_VIEW = "filialen/filialen";
 private static final String TOEVOEGEN_VIEW = "filialen/toevoegen";
+private final FiliaalService filiaalService;
+FiliaalController(FiliaalService filiaalService) {
+// Spring injecteert de parameter filiaalService met de bean die de interface
+// FiliaalService implementeert: DefaultFiliaalService
+this.filiaalService = filiaalService;
+}
 @GetMapping
 String findAll() {
 return FILIALEN_VIEW;
@@ -17,4 +32,10 @@ return FILIALEN_VIEW;
 String createForm() {
 return TOEVOEGEN_VIEW;
 }
+@PostMapping
+String create() {
+	// later voeg je een record toe aan de database
+	LOGGER.info("filiaal record toevoegen aan database");
+	return REDIRECT_URL_NA_TOEVOEGEN;
+	}
 }
