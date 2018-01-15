8166f8cec89de91ec9b90ca6d5cf9fcafc41eaff
Christopher White <cwhite01+github@gmail.com>
2018-01-15 12:13:37 +0100

    17 CLEAN URL’S MET PATH VARIABELEN

--- groenetenen/src/main/java/be/vdab/web/FiliaalController.java
+++ groenetenen/src/main/java/be/vdab/web/FiliaalController.java
@@ -4,6 +4,7 @@ import java.util.logging.Logger;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -46,8 +47,8 @@ class FiliaalController {
 		return REDIRECT_URL_NA_TOEVOEGEN;
 	}
 
-	@GetMapping(params = "id")
-	ModelAndView read(long id) {
+	@GetMapping("{id}")
+	ModelAndView read(@PathVariable long id) {
 		ModelAndView modelAndView = new ModelAndView(FILIAAL_VIEW);
 		filiaalService.read(id).ifPresent(filiaal -> modelAndView.addObject(filiaal));
 		return modelAndView;
--- groenetenen/src/main/webapp/WEB-INF/JSP/filialen/filialen.jsp
+++ groenetenen/src/main/webapp/WEB-INF/JSP/filialen/filialen.jsp
@@ -1,4 +1,5 @@
 <%@page contentType='text/html' pageEncoding='UTF-8' session='false'%>
+<%@taglib prefix='spring' uri='http://www.springframework.org/tags'%>
 <%@taglib prefix='c' uri='http://java.sun.com/jsp/jstl/core'%>
 <%@taglib prefix='v' uri='http://vdab.be/tags'%>
 <!doctype html>
@@ -8,9 +9,9 @@
 <v:menu/>
 <h1>Filialen</h1>
 <c:forEach items='${filialen}' var='filiaal'>
-<c:url var='url' value='/filialen'>
-<c:param name='id' value='${filiaal.id}'/>
-</c:url>
+<spring:url var='url' value='/filialen/{id}'>
+<spring:param name='id' value='${filiaal.id}'/>
+</spring:url>
 <h2><a href='${url}'>${filiaal.naam}</a></h2>
 <p>${filiaal.adres.straat} ${filiaal.adres.huisNr}<br>
 ${filiaal.adres.postcode} ${filiaal.adres.gemeente}</p>
